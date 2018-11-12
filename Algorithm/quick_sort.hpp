d6c1933956b7eeb1dc3149297a651ebd47b67f04
HarryShaunWang <harryshaunwang@outlook.com>
2018-11-12 17:28:20 +0800

    add quick_sort.hpp

---
+++ Algorithm/quick_sort.hpp
@@ -0,0 +1,33 @@
+/*
+    Programed by HarryShaunWang
+    Created on 2018.11.12
+    Last modified on 2018.11.12
+*/
+#ifndef QUICK_SORT_HPP
+#define QUICK_SORT_HPP
+
+#include <algorithm>
+
+template <typename T>
+inline void quickSort (T *l, T *r)
+{
+    T *i = l, *j = r, mid = * (l + ( (r - l) >> 1));
+    while (i <= j)
+    {
+        while (*i < mid)
+            ++i;
+        while (mid < *j)
+            --j;
+        if (i <= j)
+        {
+            std::swap (*i, *j);
+            ++i, --j;
+        }
+        if (l < j)
+            quickSort (l, j);
+        if (i < r)
+            quickSort (i, r);
+    }
+}
+
+#endif
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Record all the algorithm and data-structure that I have learned and I am learnin
 
 2018.11.1 Binary Indexed Tree  
 2018.11.1 Disjoint Set
+2018.11.12 Quick Sort(NOT supported iterator yet)
